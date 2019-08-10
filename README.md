75d15af27a4b66a4c4414bd59a9f171739e84077
kingoftheconnors <Connormcp777@gmail.com>
2019-08-10 18:44:40 -0400

    Added turtle to pettables and removed flying units from petters

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # Pet-The-Dog
-Petting is now a legitimate action that regains health (well, not yet, it WILL be)
+Petting is now a legitimate action that regains health
 
-##Installation
+The petter gains 15% health back for petting, while the dog being pet regains 5% (Caesar CAN be pet)
+
+Possible pet-ers:
+* Villagers
+* Soldiers
+* Spearman
+* Archers
+* Knights
+* Mages
+* Merman
+* Commanders (except Caesar. Dogs petting dogs? Ridiculous!)
+
+Possible pet-ees:
+* Dogs
+* Caesar
+* Turtles (they're so cute!)
+
+## Installation
 
 Download the zip from Github and extract the contents into your steamapp mod folder (can be found in `C:\Program Files (x86)\Steam\steamapps\common\Wargroove\modpacker_bin`). Drag the mod folder onto your wargroove-mod-packer.exe to install the mod.
--- assets_src/lua/verbs/pet.lua
+++ assets_src/lua/verbs/pet.lua
@@ -3,6 +3,8 @@ local Verb = require "wargroove/verb"
 
 
 local Pet = Verb:new()
+local healAmount = 15
+local dogHealAmount = 5
 
 function Pet:getMaximumRange(unit, endPos)
     return 1
@@ -18,13 +20,15 @@ function Pet:canExecuteWithTarget(unit, endPos, targetPos, strParam)
     end
 
     local targetUnit = Wargroove.getUnitAt(targetPos)
-    return targetUnit ~= nil and (targetUnit.unitClass.id == "dog" or targetUnit.unitClass.id == "commander_tolstoy" or targetUnit.unitClass.id == "commander_caesar")
+
+    return targetUnit ~= nil and Wargroove.areAllies(unit.playerId, targetUnit.playerId) and (targetUnit.unitClass.id == "dog" or targetUnit.unitClass.id == "turtle" or targetUnit.unitClass.id == "commander_caesar")
 end
 
 function Pet:execute(unit, targetPos, strParam, path)
     local amountToMove = 0.25
     local xMove = (targetPos.x > unit.pos.x and amountToMove) or (targetPos.x < unit.pos.x and -amountToMove) or 0
     local yMove = (targetPos.y > unit.pos.y and amountToMove) or (targetPos.y < unit.pos.y and -amountToMove) or 0
+    local targetUnit = Wargroove.getUnitAt(targetPos)
 
     local facingOverride = ""
     if targetPos.x > unit.pos.x then
@@ -44,6 +48,12 @@ function Pet:execute(unit, targetPos, strParam, path)
 
     Wargroove.spawnMapAnimation(unit.pos, 0, "fx/heal_unit")
     Wargroove.spawnMapAnimation(targetPos, 0, "fx/heal_unit")
+    Wargroove.playPositionlessSound("caesar/caesarShoutExcited")
+    --Wargroove.playPositionlessSound("caesar/caesarSoldierHeartFoley")
+
+    unit:setHealth(unit.health + healAmount, unit.id)
+    targetUnit:setHealth(targetUnit.health + dogHealAmount, unit.id)
+    Wargroove.updateUnit(targetUnit)
 
     Wargroove.waitTime(0.5)
 
@@ -56,14 +66,5 @@ function Pet:execute(unit, targetPos, strParam, path)
 
     Wargroove.unsetFacingOverride(unit.id)
 end
-
--- function Pet:generateOrders(unitId, canMove)
---     return {}
--- end
-
-
--- function Pet:getScore(unitId, order)        
---     return {score = 0, introspection = introspection}
--- end
 
 return Pet
